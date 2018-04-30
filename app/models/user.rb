3cb2b0cdd8f387f27d2a63af69cf777fd25ba8b5
bulnab <bulnab@gmail.com>
2018-04-30 22:12:37 +0300

    Add password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  attr_accessor :name
+
+  validates :phone, presence: true, length: { minimum: 8, maximum: 255 }
+  validates :password, presence: true, length: { minimum: 8, maximum: 255 }
+
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,16 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(
+      phone: '+71231212123',
+      password: 'password',
+      password_confirmation: 'password',
+    )
+  end
+
+  test 'user should be valid' do,
+    assert @user.valid?
+  end
 end
