cc0585ef03111ebc594b1e8bd5cc5b545002b902
bulnab <bulnab@gmail.com>
2018-05-05 16:42:41 +0300

    Move back to a clean state

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  attr_accessor :name
-
-  validates :phone, presence: true, length: { minimum: 8, maximum: 255 }
-  validates :password, presence: true, length: { minimum: 8, maximum: 255 }
-
-  has_secure_password
-end
