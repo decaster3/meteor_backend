d19201ef73a433834af81ebc050ad658d2f7cebe
bulnab <bulnab@gmail.com>
2018-05-05 19:00:37 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  get 'users/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Required by Devise
   root to: 'home#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
