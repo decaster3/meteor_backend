dcfc782e23a8fa445e095d74567210f9ff9c579d
bulnab <bulnab@gmail.com>
2018-05-09 22:37:40 +0300

    Add JTI column to user model

--- db/migrate/20180501174308_add_phone_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddPhoneToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :phone, :string, null: false, default: ''
-    add_index :users, :phone, unique: true
-  end
-end
