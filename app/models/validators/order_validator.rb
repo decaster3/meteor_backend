9d68174066f89bafe8ce790b7c12781c53af5567
Ринат Гумаров <tiran678@icloud.com>
2018-07-17 02:52:51 +0300

    Add validation to order creation

--- app/models/validators/order_validator.rb
+++ app/models/validators/order_validator.rb
@@ -1,8 +1,20 @@
 class OrderValidator < ActiveModel::Validator
   def validate(record)
+    meteors_plus_amount_equal_total(record)
+    user_have_enough_meteors(record)
+  end
+
+  def meteors_plus_amount_equal_total(record)
     if record.meteors + record.amount != record.total
       record.errors[:base] <<
-        "Different values. meteors(#{record.meteors}) + amount(#{record.amount}) != total(#{record.total})"
+          "Different values. meteors(#{record.meteors}) + amount(#{record.amount}) != total(#{record.total})"
+    end
+  end
+
+  def user_have_enough_meteors(record)
+    if record.meteors < record.user.meteors.group(:city_id).sum(:value)[record.city.id]
+      record.errors[:base] <<
+          "Does not have enough meteors"
     end
   end
 end
