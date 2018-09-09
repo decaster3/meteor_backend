048ed5d4fa732c746cd7df508638726caff55cef
Ринат Гумаров <tiran678@icloud.com>
2018-09-09 17:41:25 +0300

    Add api prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,30 @@
 Rails.application.routes.draw do
-  get '/pay', to: 'pay#epay'
-  post '/pay/process', to: 'pay#process_payment'
-  resources :promotions
-  resources :orders
+  scope 'api', :format => :json do
+    get '/pay', to: 'pay#epay'
+    post '/pay/process', to: 'pay#process_payment'
+    resources :promotions
+    resources :orders
 
-  resources :countries
-  resources :cities
+    resources :countries
+    resources :cities
 
-  resources :products do
-    resources :product_instances
+    resources :products do
+      resources :product_instances
+    end
+
+    resources :categories do
+      resources :subcategories
+    end
+
+    resources :users, only: [:index, :show]
+
+    get 'confirmation', to: 'confirmations#show'
+
+    devise_for :users, path: 'auth', controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}, defaults: {format: :json}
+
+    # Required by Devise
+    root to: 'home#index'
   end
-
-  resources :categories do
-    resources :subcategories
-  end
-
-  resources :users, only: [:index, :show]
-
-  get 'confirmation', to: 'confirmations#show'
-
-  devise_for :users, path: 'auth', controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }, defaults: {format: :json}
-
-  # Required by Devise
-  root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
